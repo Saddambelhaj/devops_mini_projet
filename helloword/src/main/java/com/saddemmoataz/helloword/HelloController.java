f9d51a7e4457044057d0ba2ca24286b0f1980157
saddambelhaj <saddamhajtaher@gmail.com>
2024-11-03 20:39:05 +0100

    add required dependency

--- helloword/src/main/java/com/saddemmoataz/helloword/HelloController.java
+++ helloword/src/main/java/com/saddemmoataz/helloword/HelloController.java
@@ -1,5 +1,8 @@
 package com.saddemmoataz.helloword;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 @RestController
 public class HelloController {
 
